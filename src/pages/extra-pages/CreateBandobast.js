27e0573c8801cb5c989f4582cc94c2cdb01b1023
utsavk28 <utsavkhatu416@gmail.com>
2023-04-12 18:42:39 +0530

    Minor Changes

--- src/pages/extra-pages/CreateBandobast.js
+++ src/pages/extra-pages/CreateBandobast.js
@@ -1,104 +1,108 @@
-import MainCard from "components/MainCard";
-import React, { useState } from "react";
+import MainCard from 'components/MainCard';
+import React, { useState } from 'react';
 import {
-  Button,
-  Grid,
-  TextField,
-  Box,
-} from "../../../node_modules/@mui/material/index";
+    Button,
+    Grid,
+    TextField,
+    Box,
+} from '../../../node_modules/@mui/material/index';
 
-import DateTimeRangePicker from "@wojtekmaj/react-datetimerange-picker";
+import DateTimeRangePicker from '@wojtekmaj/react-datetimerange-picker';
 
-import "@wojtekmaj/react-datetimerange-picker/dist/DateTimeRangePicker.css";
-import "react-calendar/dist/Calendar.css";
-import "react-clock/dist/Clock.css";
+import '@wojtekmaj/react-datetimerange-picker/dist/DateTimeRangePicker.css';
+import 'react-calendar/dist/Calendar.css';
+import 'react-clock/dist/Clock.css';
 
 const CreateBandobast = () => {
-  const [details, setDetails] = useState({
-    name: "",
-    address: "",
-    start: "",
-    end: "",
-    radius: 0,
-  });
-
-  const [date, onDateChange] = useState([new Date(), new Date()]);
-
-  const onChange = (e) => {
-    setDetails({
-      ...details,
-      [e.target.name]: e.target.value,
+    const [details, setDetails] = useState({
+        name: '',
+        address: '',
+        start: '',
+        end: '',
+        radius: 0,
     });
-  };
 
-  const onSubmit = (e) => {
-    e.preventDefault();
-    console.log(details);
-  };
+    const [date, onDateChange] = useState([new Date(), new Date()]);
 
-  return (
-    <MainCard title="Create Bandobast">
-      <div style={{ height: "70vh" }}>
-        <Grid container>
-          <Grid item xs={12} sm={2}></Grid>
-          <Grid item xs={12} sm={8}>
-            <form>
-              <Grid container spacing={2}>
-                <Grid item xs={12} sm={6}>
-                  <TextField
-                    label="Event name"
-                    fullWidth
-                    helperText="Enter event name"
-                    margin="normal"
-                    name="name"
-                    onChange={onChange}
-                  />
+    const onChange = (e) => {
+        setDetails({
+            ...details,
+            [e.target.name]: e.target.value,
+        });
+    };
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+        console.log(details);
+        console.log(date);
+    };
+
+    return (
+        <MainCard title='Create Bandobast'>
+            <div style={{ height: '70vh' }}>
+                <Grid container>
+                    <Grid item xs={12} sm={2}></Grid>
+                    <Grid item xs={12} sm={8}>
+                        <form>
+                            <Grid container spacing={2}>
+                                <Grid item xs={12} sm={6}>
+                                    <TextField
+                                        label='Event name'
+                                        fullWidth
+                                        helperText='Enter event name'
+                                        margin='normal'
+                                        name='name'
+                                        onChange={onChange}
+                                    />
+                                </Grid>
+                                <Grid item xs={12} sm={6}>
+                                    <TextField
+                                        label='Bandobast Radius'
+                                        defaultValue='10'
+                                        helperText='Please select Bandobast Radius (in meters)'
+                                        fullWidth
+                                        margin='normal'
+                                        name='radius'
+                                        onChange={onChange}
+                                    ></TextField>
+                                </Grid>
+                                <Grid item xs={12} sm={12}>
+                                    <TextField
+                                        label='Bandobast Location details'
+                                        fullWidth
+                                        multiline
+                                        rows={2}
+                                        helperText='Please Enter Bandobast Address'
+                                        margin='normal'
+                                        name='address'
+                                        onChange={onChange}
+                                    />
+                                </Grid>
+                                <Grid item xs={12} sm={6}>
+                                    <DateTimeRangePicker
+                                        onChange={onDateChange}
+                                        value={date}
+                                    />
+                                </Grid>
+                                <Grid item xs={12} sm={8}></Grid>
+                                <Grid item xs={12} sm={4}>
+                                    <Button
+                                        fullWidth
+                                        variant='contained'
+                                        color='primary'
+                                        onClick={onSubmit}
+                                    >
+                                        Create Event
+                                    </Button>
+                                </Grid>
+                            </Grid>
+                            <br />
+                        </form>
+                    </Grid>
                 </Grid>
-                <Grid item xs={12} sm={6}>
-                  <TextField
-                    label="Bandobast Radius"
-                    defaultValue="10"
-                    helperText="Please select Bandobast Radius (in meters)"
-                    fullWidth
-                    margin="normal"
-                    name="radius"
-                    onChange={onChange}
-                  ></TextField>
-                </Grid>
-                <Grid item xs={12} sm={12}>
-                  <TextField
-                    label="Bandobast Location details"
-                    fullWidth
-                    multiline
-                    rows={2}
-                    helperText="Please Enter Bandobast Address"
-                    margin="normal"
-                    name="address"
-                    onChange={onChange}
-                  />
-                </Grid>
-                <Grid item xs={12} sm={6}>
-                  <DateTimeRangePicker onChange={onDateChange} value={date} />
-                </Grid>
-                <Grid item xs={12} sm={8}></Grid>
-                <Grid item xs={12} sm={4}>
-                  <Button
-                    fullWidth
-                    variant="contained"
-                    color="primary"
-                    onClick={onSubmit}
-                  >
-                    Create Event
-                  </Button>
-                </Grid>
-              </Grid>
-              <br />
-            </form>
-          </Grid>
-        </Grid>
-      </div>
-    </MainCard>
-  );
+            </div>
+        </MainCard>
+    );
 };
 
 export default CreateBandobast;
--- src/pages/extra-pages/DetailedBandobast.js
+++ src/pages/extra-pages/DetailedBandobast.js
@@ -1,115 +1,118 @@
-import DetailedMapComponent from "components/DetailedMapComponent";
-import MainCard from "components/MainCard";
-import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import DetailedMapComponent from 'components/DetailedMapComponent';
+import MainCard from 'components/MainCard';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 import {
-  Box,
-  Button,
-  Divider,
-  Grid,
-  List,
-  ListItem,
-  ListItemText,
-  TextField,
-} from "../../../node_modules/@mui/material/index";
+    Box,
+    Button,
+    Divider,
+    Grid,
+    List,
+    ListItem,
+    ListItemText,
+    TextField,
+} from '../../../node_modules/@mui/material/index';
 
 const DetailedBandobast = () => {
-  const { id } = useParams();
-  const [details, setDetails] = useState(null);
+    const { id } = useParams();
+    const [details, setDetails] = useState(null);
 
-  useEffect(() => {
-    (async () => {
-      try {
-        const res = await fetch(`http://localhost:5000/api/events/${id}`);
-        const { event } = await res.json();
-        setDetails(event);
-      } catch (error) {
-        console.log(error);
-      }
-    })();
-  }, []);
-
-  return details === null ? (
-    <></>
-  ) : (
-    <MainCard title={`Details - ${details.name}`}>
-      <Grid container spacing={2}>
-        <Grid item xs={12} sm={7}>
-          <DetailedMapComponent details={details} />
-        </Grid>
-        <Grid item xs={12} sm={3}>
-          <Box
-            sx={{
-              padding: "0 10px",
-              border: "1px solid lightgrey",
-              borderRadius: "10px",
-            }}
-          >
-            <h3>Descripton</h3>
-            <Divider />
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
-              mollitia, molestiae quas vel sint commodi repudiandae consequuntur
-              voluptatum laborum numquam blanditiis harum quisquam eius sed odit
-              fugiat iusto fuga praesentium optio, eaque rerum! Provident
-              similique accusantium nemo autem.
-            </p>
-          </Box>
-          <br />
-          <Box
-            sx={{
-              padding: "0 10px",
-              paddingBottom: "10px",
-              border: "1px solid lightgrey",
-              borderRadius: "10px",
-            }}
-          >
-            <h3>Notification</h3>
-            <TextField
-              variant="filled"
-              fullWidth
-              multiline
-              rows={3}
-              label="Enter Notification"
-              helperText="Broadcast message to all personnel"
-              gutterBottom
-            ></TextField>
-            <br />
-            <Button fullWidth variant="contained">
-              Notify!
-            </Button>
-          </Box>
-        </Grid>
-        <Grid item xs={12} sm={2}>
-          <Box
-            sx={{
-              border: "1px solid lightgrey",
-              borderRadius: "10px",
-              height: "70vh",
-              padding: "0 10px",
-              overflowY: "scroll",
-            }}
-          >
-            <h3>All Personnels</h3>
-            <Divider />
-
-            <List dense>
-              {details.personnels.map((personnel, idx) => {
-                return (
-                  <ListItem key={idx}>
-                    <ListItemText
-                      primary={`${personnel.firstName} ${personnel.lastName}`}
-                      secondary={`Batch: ${personnel.batch}ID: ${personnel.id_number}`}
-                    />
-                  </ListItem>
+    useEffect(() => {
+        (async () => {
+            try {
+                const res = await fetch(
+                    `http://localhost:5000/api/events/${id}`
                 );
-              })}
-            </List>
-          </Box>
-        </Grid>
-      </Grid>
-    </MainCard>
-  );
+                const { event } = await res.json();
+                setDetails(event);
+            } catch (error) {
+                console.log(error);
+            }
+        })();
+    }, []);
+
+    return details === null ? (
+        <></>
+    ) : (
+        <MainCard title={`Details - ${details.name}`}>
+            <Grid container spacing={2}>
+                <Grid item xs={12} sm={7}>
+                    <DetailedMapComponent details={details} />
+                </Grid>
+                <Grid item xs={12} sm={3}>
+                    <Box
+                        sx={{
+                            padding: '0 10px',
+                            border: '1px solid lightgrey',
+                            borderRadius: '10px',
+                        }}
+                    >
+                        <h3>Descripton</h3>
+                        <Divider />
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing
+                            elit. Maxime mollitia, molestiae quas vel sint
+                            commodi repudiandae consequuntur voluptatum laborum
+                            numquam blanditiis harum quisquam eius sed odit
+                            fugiat iusto fuga praesentium optio, eaque rerum!
+                            Provident similique accusantium nemo autem.
+                        </p>
+                    </Box>
+                    <br />
+                    <Box
+                        sx={{
+                            padding: '0 10px',
+                            paddingBottom: '10px',
+                            border: '1px solid lightgrey',
+                            borderRadius: '10px',
+                        }}
+                    >
+                        <h3>Notification</h3>
+                        <TextField
+                            variant='filled'
+                            fullWidth
+                            multiline
+                            rows={3}
+                            label='Enter Notification'
+                            helperText='Broadcast message to all personnel'
+                            gutterBottom
+                        ></TextField>
+                        <br />
+                        <Button fullWidth variant='contained'>
+                            Notify!
+                        </Button>
+                    </Box>
+                </Grid>
+                <Grid item xs={12} sm={2}>
+                    <Box
+                        sx={{
+                            border: '1px solid lightgrey',
+                            borderRadius: '10px',
+                            height: '70vh',
+                            padding: '0 10px',
+                            overflowY: 'scroll',
+                        }}
+                    >
+                        <h3>All Personnels</h3>
+                        <Divider />
+
+                        <List dense>
+                            {details.personnels.map((personnel, idx) => {
+                                return (
+                                    <ListItem key={idx}>
+                                        <ListItemText
+                                            primary={`${personnel.firstName} ${personnel.lastName}`}
+                                            secondary={`Batch: ${personnel.batch}ID: ${personnel.id_number}`}
+                                        />
+                                    </ListItem>
+                                );
+                            })}
+                        </List>
+                    </Box>
+                </Grid>
+            </Grid>
+        </MainCard>
+    );
 };
 
 export default DetailedBandobast;
