2818461380bda0870261ead4621601f3ac824a27
Tushar Bauskar <btushar1208@gmail.com>
2023-04-12 16:05:32 +0530

    View bandobast integration done

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -1,49 +1,50 @@
 import MainCard from "components/MainCard";
 import {
-  Box,
   Divider,
   Grid,
   List,
   ListItem,
   ListItemText,
-  Paper,
-  Typography,
 } from "../../../node_modules/@mui/material/index";
 import MapComponent from "components/MapComponent";
+import { useEffect, useState } from "react";
 
 // ==============================|| DASHBOARD - DEFAULT ||============================== //
 
 const DashboardDefault = () => {
+  const [events, setEvents] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const res = await fetch(
+          "http://localhost:5000/api/events/active-events"
+        );
+        const data = await res.json();
+        setEvents(data);
+        console.log(data);
+      } catch (error) {
+        console.log(error);
+      }
+    })();
+  }, []);
+
   return (
     <MainCard title="View Bandobast">
       <Grid container spacing={4}>
         <Grid item xs={12} sm={2}>
-          <h3>Active Bandobasts</h3>
+          <h3>Active Events</h3>
           <Divider></Divider>
-          <List>
-            <ListItemText primary="Bandobast 1" secondary="Lorem Ipsum" />
-          </List>
-          <List>
-            <ListItemText primary="Bandobast 2" secondary="Lorem Ipsum" />
-          </List>
-          <List>
-            <ListItemText primary="Bandobast 3" secondary="Lorem Ipsum" />
-          </List>
-          <List>
-            <ListItemText primary="Bandobast 4" secondary="Lorem Ipsum" />
-          </List>
-          <List>
-            <ListItemText primary="Bandobast 5" secondary="Lorem Ipsum" />
-          </List>
-          <List>
-            <ListItemText primary="Bandobast 5" secondary="Lorem Ipsum" />
-          </List>
-          <List>
-            <ListItemText primary="Bandobast 6" secondary="Lorem Ipsum" />
-          </List>
+          {events.map((event) => {
+            return (
+              <List>
+                <ListItemText primary={event.name} secondary={event.start} />
+              </List>
+            );
+          })}
         </Grid>
         <Grid item xs={12} sm={10}>
-          <MapComponent />
+          <MapComponent events={events} />
         </Grid>
       </Grid>
     </MainCard>
