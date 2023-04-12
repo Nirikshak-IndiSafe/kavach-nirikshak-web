39d640d5b6b34f0c82958f0ee04526414e2b2a57
Asavari Ambavane <ambavaneasavari@gmail.com>
2023-04-12 17:35:51 +0530

    date time integrated

--- src/pages/extra-pages/CreateBandobast.js
+++ src/pages/extra-pages/CreateBandobast.js
@@ -4,7 +4,14 @@ import {
   Button,
   Grid,
   TextField,
+  Box,
 } from "../../../node_modules/@mui/material/index";
+
+import DateTimeRangePicker from "@wojtekmaj/react-datetimerange-picker";
+
+import "@wojtekmaj/react-datetimerange-picker/dist/DateTimeRangePicker.css";
+import "react-calendar/dist/Calendar.css";
+import "react-clock/dist/Clock.css";
 
 const CreateBandobast = () => {
   const [details, setDetails] = useState({
@@ -14,6 +21,8 @@ const CreateBandobast = () => {
     end: "",
     radius: 0,
   });
+
+  const [date, onDateChange] = useState([new Date(), new Date()]);
 
   const onChange = (e) => {
     setDetails({
@@ -29,60 +38,65 @@ const CreateBandobast = () => {
 
   return (
     <MainCard title="Create Bandobast">
-      <Grid container>
-        <Grid item xs={12} sm={2}></Grid>
-        <Grid item xs={12} sm={8}>
-          <form>
-            <Grid container spacing={2}>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  label="Event name"
-                  fullWidth
-                  helperText="Enter event name"
-                  margin="normal"
-                  name="name"
-                  onChange={onChange}
-                />
+      <div style={{ height: "70vh" }}>
+        <Grid container>
+          <Grid item xs={12} sm={2}></Grid>
+          <Grid item xs={12} sm={8}>
+            <form>
+              <Grid container spacing={2}>
+                <Grid item xs={12} sm={6}>
+                  <TextField
+                    label="Event name"
+                    fullWidth
+                    helperText="Enter event name"
+                    margin="normal"
+                    name="name"
+                    onChange={onChange}
+                  />
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                  <TextField
+                    label="Bandobast Radius"
+                    defaultValue="10"
+                    helperText="Please select Bandobast Radius (in meters)"
+                    fullWidth
+                    margin="normal"
+                    name="radius"
+                    onChange={onChange}
+                  ></TextField>
+                </Grid>
+                <Grid item xs={12} sm={12}>
+                  <TextField
+                    label="Bandobast Location details"
+                    fullWidth
+                    multiline
+                    rows={2}
+                    helperText="Please Enter Bandobast Address"
+                    margin="normal"
+                    name="address"
+                    onChange={onChange}
+                  />
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                  <DateTimeRangePicker onChange={onDateChange} value={date} />
+                </Grid>
+                <Grid item xs={12} sm={8}></Grid>
+                <Grid item xs={12} sm={4}>
+                  <Button
+                    fullWidth
+                    variant="contained"
+                    color="primary"
+                    onClick={onSubmit}
+                  >
+                    Create Event
+                  </Button>
+                </Grid>
               </Grid>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  label="Bandobast Radius"
-                  defaultValue="10"
-                  helperText="Please select Bandobast Radius (in meters)"
-                  fullWidth
-                  margin="normal"
-                  name="radius"
-                  onChange={onChange}
-                ></TextField>
-              </Grid>
-              <Grid item xs={12} sm={12}>
-                <TextField
-                  label="Bandobast Location details"
-                  fullWidth
-                  multiline
-                  rows={2}
-                  helperText="Please Enter Bandobast Address"
-                  margin="normal"
-                  name="address"
-                  onChange={onChange}
-                />
-              </Grid>
-              <Grid item xs={12} sm={8}></Grid>
-              <Grid item xs={12} sm={4}>
-                <Button
-                  fullWidth
-                  variant="contained"
-                  color="primary"
-                  onClick={onSubmit}
-                >
-                  Create Event
-                </Button>
-              </Grid>
-            </Grid>
-            <br />
-          </form>
+              <br />
+            </form>
+          </Grid>
         </Grid>
-      </Grid>
+      </div>
     </MainCard>
   );
 };
